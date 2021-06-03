7cc18b6f137dbf477ccb9a081c0ebb9ad2153271
Aditya V <adityaprasadv@gmail.com>
2021-06-03 10:33:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Howdy "User"!</p>
+        <p>Howdy Aditya!</p>
       </header>
     </div>
   );
